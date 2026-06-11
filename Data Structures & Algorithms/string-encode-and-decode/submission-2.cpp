14a47a8cb2fc260bf1584cfe8ede9da1b46747d0
ankitaroy68 <40118486+ankitaroy68@users.noreply.github.com>
2026-06-11 10:35:12 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res="";
+        for(string s:strs) res+=to_string(s.size())+"#"+s;
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        int i=0;
+        while(i<s.length()){
+            string len = "";
+            while(s[i]!='#') len+=s[i++];
+            string cur(stoi(len),' ');
+            i+=1;
+            int j=0;
+            while(j<cur.size()) cur[j++]=s[i++];
+            res.push_back(cur);
+        }
+        return res;
+    }
+};
